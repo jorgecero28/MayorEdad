32f776df636ab309a3e123f5e72ebb9cbac3d91a
jorgecero28 <jorgecedeno28@gmail.com>
2024-01-23 23:16:15 -0500

    Proyecto Java

---
+++ MayorEdad.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package mayoredad;
+
+/**
+ *
+ * @author jorge
+ */
+public class MayorEdad {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int edad=20;
+        
+        if(edad>=18){
+            System.out.println("Tu edad es mayor de edad");
+        }
+        else{
+            System.out.println("Eres menor de edad");
+        }
+    }
+    
+}
